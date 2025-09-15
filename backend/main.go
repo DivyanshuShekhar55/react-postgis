519f9fa89c89b8e8704fedf5dbcbbd6df356028b
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-15 23:21:20 +0530

    updated main file

--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 )
 
+type dbconfig struct {
+	conn *pgx.Conn
+	// maybe define some max conections (active/idle)
+	// but abhi ke liye itna hi kaafi hai
+}
+
+type config struct {
+	addr string
+	db   dbconfig
+	env  string
+}
+
+type Application struct {
+	conf config
+}
+
 func main() {
 	pg_conn_str := os.Getenv("PG_CONN")
+	env := os.Getenv("ENV")
+
 	if pg_conn_str == "" {
 		fmt.Print("couldnt find conn string")
 		return
@@ -24,5 +44,33 @@ func main() {
 
 	defer db.Close(ctx)
 
-	
+	pg := dbconfig{
+		conn: db,
+	}
+
+	conf := config{
+		addr: ":6969",
+		db:   pg,
+		env:  env,
+	}
+
+	app := Application{
+		conf: conf,
+	}
+
+	srv := http.Server{
+		Addr:         app.conf.addr,
+		IdleTimeout:  time.Second * 30,
+		WriteTimeout: time.Second * 60,
+		ReadTimeout:  time.Second * 30,
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		fmt.Printf("error starting server %v", err)
+		panic(err)
+	}
+
+	defer srv.Close()
+
 }
