43416cf855b185826479b8ad86ff63c67a8fe195
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-15 21:21:22 +0530

    pg setup

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,27 @@
-package main 
+package main
 
-func main(){
-	
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	pg_conn_str := os.Getenv("PG_CONN")
+	if pg_conn_str == "" {
+		fmt.Print("couldnt find conn string")
+		return
+	}
+
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, pg_conn_str)
+	if err != nil {
+		fmt.Printf("couldn't connect to pg %s", err)
+		return
+	}
+
+	defer conn.Close(ctx)
 }
