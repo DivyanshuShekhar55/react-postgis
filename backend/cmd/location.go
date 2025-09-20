c35dc09e1f17c289de9578166ab3ecb0f2fcc32f
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-20 11:32:34 +0530

    main module db location file

--- backend/cmd/location.go
+++ backend/cmd/location.go
@@ -1,7 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/DivyanshuShekhar55/backend/internal/pg"
+	"github.com/jackc/pgx/v5"
+)
+
+type DbImpl struct {
+	conn *pgx.Conn
+}
+
+func NewDbImpl(conn *pgx.Conn) *DbImpl{
+	return &DbImpl{conn: conn}
+}
+
+type point struct {
+	X float64
+	Y float64
+}
+
+type Location struct {
+	geoType   string
+	geoPoints []point
+}
 
 func (app *Application) InsertLocation(w http.ResponseWriter, r *http.Request) {
-	
+
+}
+
+func (db *DbImpl) GetAllPolygons(w http.ResponseWriter, r *http.Request){
+	 pg.GetAllPolygons()
 }
