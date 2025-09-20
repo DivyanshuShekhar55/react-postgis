cbac70a944d457fa36bd746ac4b519bb241b6e30
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-20 12:38:35 +0530

    location get finished

--- backend/cmd/location.go
+++ backend/cmd/location.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/DivyanshuShekhar55/backend/internal/pg"
@@ -11,24 +12,35 @@ type DbImpl struct {
 	conn *pgx.Conn
 }
 
-func NewDbImpl(conn *pgx.Conn) *DbImpl{
+func NewDbImpl(conn *pgx.Conn) *DbImpl {
 	return &DbImpl{conn: conn}
 }
 
-type point struct {
-	X float64
-	Y float64
-}
+// type point struct {
+// 	X float64
+// 	Y float64
+// }
 
-type Location struct {
-	geoType   string
-	geoPoints []point
-}
+// type Location struct {
+// 	geoType   string
+// 	geoPoints []point
+// }
 
 func (app *Application) InsertLocation(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (db *DbImpl) GetAllPolygons(w http.ResponseWriter, r *http.Request){
-	 pg.GetAllPolygons(r.Context(), db.conn)
+func (db *DbImpl) GetAllPolygons(w http.ResponseWriter, r *http.Request) {
+	res, err := pg.GetAllPolygons(r.Context(), db.conn)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(&res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 }
