90788924bc87207a883df0ec681be368e98f2705
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-22 11:48:29 +0530

    fetching data init

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,7 +6,15 @@ import MapView from './MapView'
 
 
 function App() {
-  
+  const [poly, setPoly] = useState()
+
+  useEffect(() => {
+    const polys = fetch('http://localhost:6969/location')
+      .then(res => res.json())
+      .then(data => setPoly(data))
+  }, [])
+
+  console.log(poly)
 
   return (
     <MapView />
