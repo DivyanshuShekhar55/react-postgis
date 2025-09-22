53a3390d3857029e21992ff170c2797a23c0f2de
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-22 11:18:49 +0530

    insert func for pg

--- backend/internal/pg/pg.go
+++ backend/internal/pg/pg.go
@@ -19,8 +19,9 @@ type GeoJSONPolygon struct {
 	Coordinates [][][]float64 `json:"coordinates"`
 }
 
-func Insert() error {
-	return nil
+type InsertBody struct {
+	Name       string      `json:"name"`
+	PolyPoints [][]float64 `json:"poly_points"`
 }
 
 func GetAllPolygons(ctx context.Context, conn *pgx.Conn) (res []PolygonRow, err error) {
@@ -57,7 +58,31 @@ func GetAllPolygons(ctx context.Context, conn *pgx.Conn) (res []PolygonRow, err 
 		})
 
 	}
-	
+
 	return res, rows.Err()
+
+}
+
+func Insert(ctx context.Context, conn *pgx.Conn, body InsertBody) error {
+	query := `INSERT INTO polygons (name, geom) VALUES ($1, ST_GeomFromGeJSON($2))`
+
+	geo_json_struct := GeoJSONPolygon{
+		Type:        "Polygon",
+		Coordinates: [][][]float64{body.PolyPoints},
+	}
+
+	geo_json_byte, err := json.Marshal(geo_json_struct)
+	if err != nil {
+		return fmt.Errorf("error marsahlling to json %s", err)
+	}
+
+	geo_json_str := string(geo_json_byte)
+
+	_, err = conn.Exec(ctx, query, body.Name, geo_json_str)
+	if err != nil {
+		return fmt.Errorf("error inserting data %s", err)
+	}
+
+	return nil
 
 }
