4526d9c838f166c67bf7137e334f8667d1d8922a
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-24 11:21:24 +0530

    proper desturcturing data

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,23 +1,33 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MapView from './MapView'
 
 
 function App() {
-  const [poly, setPoly] = useState()
+  const [poly, setPoly] = useState([])
 
   useEffect(() => {
     const polys = fetch('http://localhost:6969/location')
       .then(res => res.json())
-      .then(data => setPoly(data))
+      .then(data => data.map(shape => {
+        const {geom, id, name} = shape
+        const coords = geom.coordinates[0]
+        // coords array of array hai
+        // jaise ki [ [2.005, 3.455], [6.77, 8.99] ]
+       
+        // curently stores lang, lat in backend
+        // but leaflet js expects lat, lang pair
+        // isliye reverse the order
+        const reversedCoords = coords.map(([lng, lat]) => [lat, lng])
+
+        setPoly(prev => [...prev, reversedCoords])
+      }))
   }, [])
 
-  console.log(poly)
+  
 
   return (
-    <MapView />
+    <MapView polys={poly}/>
   )
 }
 
