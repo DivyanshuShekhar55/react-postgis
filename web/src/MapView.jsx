bd06fc0130ab7b03c464aee14dd60d342688f478
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-24 11:21:37 +0530

    map view fixed and updated

--- web/src/MapView.jsx
+++ web/src/MapView.jsx
@@ -1,43 +1,32 @@
-import React from 'react'
-import { MapContainer, TileLayer, useMap, Marker, Popup,Polygon } from 'react-leaflet'
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import './map-view.css'
-const multiPolygon = [
-  [
-    [51.51, -0.12],
-    [51.51, -0.13],
-    [51.53, -0.13],
-  ],
-  [
-    [51.51, -0.05],
-    [51.51, -0.07],
-    [51.53, -0.07],
-  ],
-]
+import './map-view.css';
 
-const purpleOptions = { color: 'purple' }
+const purpleOptions = { color: 'purple' };
 
-function MapView() {
-    const position = [51.505, -0.09]
-    return (
-        <div className='map__main'>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={true} className='leaflet-custom'>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+function MapView({ polys }) {
+  const position = [51.505, -0.09]; 
 
-                <Polygon pathOptions={purpleOptions} positions={multiPolygon} />
-            </MapContainer>
-        </div>
-    )
+  return (
+    <div className='map__main'>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={true} className='leaflet-custom'>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+
+        {polys.map((coords, idx) => (
+          <Polygon key={idx} pathOptions={purpleOptions} positions={coords} />
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
 
-
-
-export default MapView
+export default MapView;
