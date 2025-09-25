51ef1fe5cbab081c129d2c3cf2ce07ded541642f
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-09-25 09:49:43 +0530

    moved state fetching to proper place

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,33 +5,12 @@ import DataInsertionComponent from './DataInsertionComponent'
 
 
 function App() {
-  const [poly, setPoly] = useState([])
-
-  useEffect(() => {
-    const polys = fetch('http://localhost:6969/location')
-      .then(res => res.json())
-      .then(data => data.map(shape => {
-        const {geom, id, name} = shape
-        const coords = geom.coordinates[0]
-        // coords array of array hai
-        // jaise ki [ [2.005, 3.455], [6.77, 8.99] ]
-       
-        // curently stores lang, lat in backend
-        // but leaflet js expects lat, lang pair
-        // isliye reverse the order
-        const reversedCoords = coords.map(([lng, lat]) => [lat, lng])
-
-        setPoly(prev => [...prev, reversedCoords])
-      }))
-  }, [])
-
-  
 
   return (
     <>
-    
-    <DataInsertionComponent />
-    <MapView polys={poly}/>
+
+      <DataInsertionComponent />
+      <MapView />
     </>
   )
 }
--- web/src/MapView.jsx
+++ web/src/MapView.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './map-view.css';
 
 const purpleOptions = { color: 'purple' };
 
-function MapView({ polys }) {
-  const position = [51.505, -0.09]; 
+function MapView() {
+  const [poly, setPoly] = useState([])
+  const [mapData, setMapData] = useState([])
+
+  useEffect(() => {
+    const polys = fetch('http://localhost:6969/location')
+      .then(res => res.json())
+      .then(data => data.map(shape => {
+        const { geom, id, name } = shape
+        const coords = geom.coordinates[0]
+        // coords array of array hai
+        // jaise ki [ [2.005, 3.455], [6.77, 8.99] ]
+
+        // curently stores lang, lat in backend
+        // but leaflet js expects lat, lang pair
+        // isliye reverse the order
+        const reversedCoords = coords.map(([lng, lat]) => [lat, lng])
+
+        setPoly(prev => [...prev, reversedCoords])
+        setMapData(prev => [...prev, { name, id, coords: reversedCoords }])
+      }))
+  }, [])
+  const position = [51.505, -0.09];
 
   return (
     <div className='map__main'>
@@ -15,14 +36,12 @@ function MapView({ polys }) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
 
-        {polys.map((coords, idx) => (
-          <Polygon key={idx} pathOptions={purpleOptions} positions={coords} />
+
+        {mapData.map((item, idx) => (
+          <Polygon key={idx} pathOptions={purpleOptions} positions={item.coords} >
+            <Popup>Polygon {item.name}</Popup>
+          </Polygon>
         ))}
       </MapContainer>
     </div>
